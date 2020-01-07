22a698f7db90dcc9e4fb8092e0f57088ff3354f6
miia <miia@com.tw>
2020-01-07 14:01:57 +0800

    1st

--- app/src/main/java/com/fju/member/MainActivity.java
+++ app/src/main/java/com/fju/member/MainActivity.java
@@ -2,7 +2,9 @@ package com.fju.member;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
     }
+
+    public void login (View view){
+        Intent intent = new Intent(this,NicknameActivity.class);
+        startActivityForResult(intent,10);
+    }
+
 }
